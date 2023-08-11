b8d8a3933760ad77343e9ecc716adfaa85998852
EC2 Default User <ec2-user@ip-172-31-29-80.ap-northeast-1.compute.internal>
2023-08-11 14:38:59 +0000

    [Add]meshiterro2_app devise実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "c32d2dedf58c4e3eab8f2acccefea189.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
 end
